8591a5356a327e1846675cc182aae0c92d883c9a
HarshaGiribabu <harshagiri1981@gmail.com>
2024-05-23 18:15:17 +0530

    Update area.py

--- area.py
+++ area.py
@@ -1,16 +1,10 @@
 class Circle:
     def __init__(self, r):
         self.radius = r
-
     def area(self):
         return 3.141 * self.radius ** 2
-
     def perimeter(self):
         return 2 * 3.141 * self.radius
-
-# Create an object of the class
 NewCircle = Circle(7)
-
-# Call the methods to calculate area and perimeter
 print("Area:", NewCircle.area())
 print("Perimeter:", NewCircle.perimeter())
